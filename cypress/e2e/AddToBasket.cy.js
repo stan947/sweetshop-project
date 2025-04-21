6f79d7bb43a4c8187eaa0a812de41538e684c318
sta947 <stanislovas.sniksta@gmail.com>
2025-04-21 13:38:22 +0300

    Update BasketPage.cy.js

--- cypress/e2e/AddToBasket.cy.js
+++ cypress/e2e/AddToBasket.cy.js
@@ -1,5 +1,5 @@
-describe("TS_AddToBasket: Add to Basket Functionality", () => {
 
+describe("TS_AddToBasket: Add to Basket Functionality", () => {
     const selectors = {
       productCard: ".card",
       productName: ".card-title",
@@ -14,73 +14,108 @@ describe("TS_AddToBasket: Add to Basket Functionality", () => {
       basketCountNav: ".badge.badge-success",
       basketCountPage: "#basketCount",
     };
-
+  
     beforeEach(() => {
-        cy.clearLocalStorage();
-        cy.clearCookies();
-    
-      });
-    
-
-    function addRandomProductToBasket() {
-      cy.get(selectors.productCard).should('have.length.greaterThan', 0).then(($cards) => {
-        const randomIndex = Math.floor(Math.random() * $cards.length);
-        const selectedCard = $cards[randomIndex];
+      cy.clearLocalStorage();
+      cy.clearCookies();
+    });
   
-        cy.wrap(selectedCard).within(() => {
-          cy.get(selectors.productName)
-            .invoke("text")
-            .then((name) => cy.wrap(name.trim()).as("productName"));
-  
-          cy.get(selectors.productPrice)
-            .invoke("text")
-            .then((price) => cy.wrap(price.trim()).as("productPrice"));
-  
-          cy.get(selectors.addToBasketButton).click();
-        });
-  
-    
-        cy.get(selectors.basketCountNav, { timeout: 10000 }).should("contain", "1");
-        cy.wait(500);  
-      });
+    function normalizeText(text) {
+      return text
+        .toLowerCase()
+        .trim()
+        .replace(/\s+/g, " ")
+        .replace(/[^\w\s]/g, "");
     }
   
-    
+    function addRandomProductToBasket() {
+      cy.get(selectors.productCard)
+        .should("have.length.greaterThan", 0)
+        .then(($cards) => {
+          const randomIndex = Math.floor(Math.random() * $cards.length);
+          const selectedCard = $cards[randomIndex];
+  
+          cy.wrap(selectedCard).within(() => {
+            cy.get(selectors.productName)
+              .invoke("text")
+              .then((name) => {
+                cy.wrap(name).as("productName");
+              });
+  
+            cy.get(selectors.productPrice)
+              .invoke("text")
+              .then((price) => {
+                cy.wrap(price).as("productPrice");
+              });
+  
+            cy.get(selectors.addToBasketButton).click();
+          });
+  
+          cy.get(selectors.basketCountNav).should("contain", "1");
+          cy.wait(500);
+        });
+    }
+  
     function goToBasket() {
       cy.get(selectors.nav).within(() => {
         cy.contains("Basket").click();
       });
-  
-    
       cy.get(selectors.basketList, { timeout: 10000 }).should("be.visible");
     }
   
-
     function verifyProductInBasket(expectedQty = 1) {
       cy.get("@productName").then((name) => {
-        const trimmedName = name.trim();
+        const expectedName = normalizeText(name);
+        cy.log("Expected normalized product name:", expectedName);
   
         cy.get("@productPrice").then((price) => {
-          const trimmedPrice = price.trim();
+          const expectedPrice = price.trim();
+          cy.log("Expected product price:", expectedPrice);
   
-    
-          cy.get(selectors.basketItem).first().within(() => {
-            cy.get(selectors.basketItemName).should("contain", trimmedName);
-            cy.get(selectors.basketItemQuantity).should("contain", `x ${expectedQty}`);
-            cy.get(selectors.basketItemPrice).should("contain", trimmedPrice);
-          });
+          cy.get(selectors.basketItem)
+            .first()
+            .within(() => {
+              cy.get(selectors.basketItemName, { timeout: 6000 })
+                .should("exist")
+                .and("be.visible")
+                .invoke("text")
+                .then((actualName) => {
+                  const basketName = normalizeText(actualName);
+                  cy.log("Actual normalized product name in basket:", basketName);
+                  expect(basketName).to.include(expectedName);
+                });
   
-          
-          cy.get(selectors.basketCountPage).should("contain", expectedQty);
+              cy.get(selectors.basketItemQuantity)
+                .should("exist")
+                .and("be.visible")
+                .then((el) => {
+                  cy.log("Actual quantity text:", el.text());
+                  expect(el.text()).to.include(`x ${expectedQty}`);
+                });
+  
+              cy.get(selectors.basketItemPrice)
+                .should("exist")
+                .and("be.visible")
+                .then((el) => {
+                  cy.log("Actual price text:", el.text());
+                  expect(el.text()).to.include(expectedPrice);
+                });
+            });
+  
+          cy.get(selectors.basketCountPage)
+            .should("exist")
+            .and("be.visible")
+            .then((el) => {
+              cy.log("Basket count on page:", el.text());
+              expect(el.text()).to.include(`${expectedQty}`);
+            });
         });
       });
     }
   
     context("From Main Page", () => {
       beforeEach(() => {
-    
         cy.visit("/");
-    
       });
   
       it("TC_Add_1: Should add random product from main page to basket and verify", () => {
@@ -92,11 +127,8 @@ describe("TS_AddToBasket: Add to Basket Functionality", () => {
   
     context("From Sweets Page", () => {
       beforeEach(() => {
-        
         cy.visit("/sweets");
-    
-        cy.url().should('include', '/sweets');
-    
+        cy.url().should("include", "/sweets");
       });
   
       it("TC_Add_2: Should add random product from sweets page to basket and verify", () => {
@@ -108,17 +140,18 @@ describe("TS_AddToBasket: Add to Basket Functionality", () => {
   });
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
